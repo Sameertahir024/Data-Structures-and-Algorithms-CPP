e7950ef6b5c551c4f23e2201291eaff1209eebc6
sameer tahir <sameertahir024@gmail.com>
2024-09-30 11:49:04 +0500

    Patterns

---
+++ Patterns/01_Pattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+    1 2 3 4
+    1 2 3 4
+    1 2 3 4
+    1 2 3 4
+    */
+
+int n = 4;
+for (int i = 0; i < n; i++  ) {
+    for (int j = 0; j < n; j++) {
+        cout << j << " ";
+    }
+    cout << endl ;
+}
+    
+}
